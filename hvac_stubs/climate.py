bee70b74adf66705b7c4ffec0e92cd657861e46d
Tony Brobston <anthonybrobston@live.com>
2024-04-21 18:59:29 -0500

    Remove bad import

--- hvac_stubs/climate.py
+++ hvac_stubs/climate.py
@@ -1,10 +1,5 @@
 from __future__ import annotations
 
-from homeassistant.components.sensor import (
-    ClimateDeviceClass,
-    ClimateEntity,
-    ClimateStateClass,
-)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
