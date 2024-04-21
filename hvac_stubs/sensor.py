434c66bad5a850e8e7e221760e58239f7abe18dc
Tony Brobston <anthonybrobston@live.com>
2024-04-21 14:53:03 -0500

    Refactor sensors to add more temperature sensors

--- hvac_stubs/sensor.py
+++ hvac_stubs/sensor.py
@@ -17,14 +17,20 @@ def setup_platform(
     add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None
 ) -> None:
-    add_entities([TemperatureSensor()])
+    add_entities([
+        TemperatureSensor("Main Floor Temperature", 71),
+        TemperatureSensor("Basement Temperature", 69),
+        TemperatureSensor("Office Temperature", 76),
+        TemperatureSensor("Guest Bedroom Temperature", 72),
+        TemperatureSensor("Master Bedroom Temperature", 75),
+        TemperatureSensor("Upstairs Bathroom Temperature", 74),
+    ])
 
 
 class TemperatureSensor(SensorEntity):
-    _attr_name = "Example Temperature"
-    _attr_native_unit_of_measurement = UnitOfTemperature.FAHRENHEIT
-    _attr_device_class = SensorDeviceClass.TEMPERATURE
-    _attr_state_class = SensorStateClass.MEASUREMENT
-
-    def update(self) -> None:
-        self._attr_native_value = 71
+    def __init__(self, name: str, temperature: int) -> None:
+        self._attr_name = name
+        self._attr_native_unit_of_measurement = UnitOfTemperature.FAHRENHEIT
+        self._attr_device_class = SensorDeviceClass.TEMPERATURE
+        self._attr_state_class = SensorStateClass.MEASUREMENT
+        self._attr_native_value = temperature
